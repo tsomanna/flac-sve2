93be3cdab49e84727ddfa1e3d9b8ec5049ca2181
tsomanna_QCOM <tsomanna@qti.qualcomm.com>
2026-08-09 12:16:40 +0530

    Enable actions

--- src/libFLAC/bitreader_intrin_sve2.c
+++ src/libFLAC/bitreader_intrin_sve2.c
@@ -48,5 +48,5 @@
 
 FLAC__bool FLAC__bitreader_read_rice_signed_block_sve2(FLAC__BitReader *br, int vals[], uint32_t nvals, uint32_t parameter)
 #include "deduplication/bitreader_read_rice_signed_block.c"
-
+//actions
 #endif /* FLAC__SVE2_SUPPORTED */
